5015e0cb3c8cd5877581350fe48fc9bcbc0acee7
Joseph Crispell <crispelljoseph@gmail.com>
2023-07-26 19:14:57 +0100

    Adding TODO tags

--- python_coverage_badge/__main__.py
+++ python_coverage_badge/__main__.py
@@ -4,6 +4,8 @@ def main():
 
 
 # TODO move code from ../main.py to here and get working
+# TODO check working directory and whether depends on where package called
+# TODO update README
 
 if __name__ == "__main__":
     main()
