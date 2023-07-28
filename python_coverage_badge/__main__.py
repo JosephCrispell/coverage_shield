5ae57ad147429f8f4e2038c2ee35c84d1d47f520
Joseph Crispell <crispelljoseph@gmail.com>
2023-07-28 20:44:23 +0100

    Moved main to __main__.py

--- python_coverage_badge/__main__.py
+++ python_coverage_badge/__main__.py
@@ -1,11 +1,35 @@
-def main():
+# Load packages
+from pathlib import Path  # handling file paths
 
-    print("hello")
+# Local imports
+from python_coverage_badge import (
+    unittest_coverage_functions,
+)  # functions for running coverage
 
-
+# TODO update to change badge when unittests fail - red "failing" instead of value?
+# TODO add more colour categories to scale
 # TODO move code from ../main.py to here and get working
 # TODO check working directory and whether depends on where package called
 # TODO update README
+
+
+def main():
+
+    # Run code coverage
+    coverage_dataframe = unittest_coverage_functions.run_code_coverage()
+
+    # Build badge
+    coverage_badge_url = unittest_coverage_functions.make_coverage_badge_url(
+        coverage_dataframe
+    )
+
+    # Update badge in README
+    unittest_coverage_functions.replace_regex_in_file(
+        file_path=Path("README.md"),
+        pattern_regex=r"\!\[Code Coverage\]\(.+\)",
+        replacement=f"![Code Coverage]({coverage_badge_url})",
+    )
+
 
 if __name__ == "__main__":
     main()
