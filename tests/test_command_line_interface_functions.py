56d39f9c88e575f36efa485ca27e6a4be3c9c91d
Joseph Crispell <crispelljoseph@gmail.com>
2023-08-05 19:03:21 +0100

    Added tests for command line functions

---
+++ tests/test_command_line_interface_functions.py
@@ -0,0 +1,55 @@
+# Load packages
+import unittest  # running tests
+from pathlib import Path  # handling file paths
+
+# Local imports
+from python_coverage_badge import command_line_interface_functions  # cli functions
+
+
+class TestCommandLineInterfaceFunctions(unittest.TestCase):
+    def test_build_command_line_interface(self):
+        """Test command line parser is built"""
+
+        # Build the command line interface parser
+        parser = command_line_interface_functions.build_command_line_interface()
+
+        # Check argument parser returned
+        self.assertEqual(
+            str(type(parser)),
+            "<class 'argparse.ArgumentParser'>",
+            "Check argument parser returned",
+        )
+
+    def test_parse_command_line_arguments(self):
+
+        # Build the command line interface parser
+        parser = command_line_interface_functions.build_command_line_interface()
+
+        # Define the command line arguments and parse
+        directory = "test_directory"
+        readme = "test_README"
+        arguments = [
+            "--directory",
+            directory,
+            "--readme",
+            readme,
+        ]
+        args = command_line_interface_functions.parse_command_line_arguments(
+            parser, arguments, testing=True
+        )
+
+        # Check args exist
+        self.assertEqual(
+            directory,
+            args.directory,
+            "Check directory stored as argument",
+        )
+        self.assertEqual(
+            readme,
+            args.readme,
+            "Check readme stored as argument",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
