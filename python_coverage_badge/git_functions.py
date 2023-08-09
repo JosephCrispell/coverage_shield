5abbc77ae80f9f826d0ed23e8a847810ae489117
Joseph Crispell <crispelljoseph@gmail.com>
2023-08-09 19:45:43 +0100

    Added check for if file changed

--- python_coverage_badge/git_functions.py
+++ python_coverage_badge/git_functions.py
@@ -3,6 +3,40 @@ import subprocess  # command line commands
 from pathlib import Path  # handling file paths
 
 
+def check_if_file_changed_using_git(file_path: Path) -> bool:
+    """Use git to check if file provided has changed in repo
+
+    Args:
+        file_path (Path): path to file that want to check
+
+    Raises:
+        subprocess.CalledProcessError: throws error if git status command fails
+
+    Returns:
+        bool: True if files have changed and False otherwise
+    """
+    # Run git status command
+    git_status_command = ["git", "status", str(file_path)]
+    command_result = subprocess.run(git_status_command, capture_output=True, text=True)
+
+    # Check if ran ok
+    if command_result.returncode == 0:  # Passing
+
+        # Check if there is anything to commit
+        if "nothing to commit" in command_result.stdout:
+            return False
+    else:
+        raise subprocess.CalledProcessError(
+            returncode=command_result.returncode,
+            cmd=command_result.args,
+            output=command_result.stdout,
+            stderr=command_result.stderr,
+        )
+
+    # If got to here there must be changes to commit
+    return True
+
+
 def push_updated_readme(readme_path: Path = Path("README.md")):
     """Uses git to stage, commit, and push changes to README.md (updated badge)
 
@@ -10,14 +44,17 @@ def push_updated_readme(readme_path: Path = Path("README.md")):
         readme_path (Path, optional): path to README.md file. Defaults to Path("README.md").
     """
 
-    # Stage the changes (updated badge)
-    stage_file(readme_path)
+    # Check if updated README changed
+    if check_if_file_changed_using_git(readme_path):
 
-    # Commit changes
-    commit_changes(message=f"Updated coverage badge in {readme_path}")
+        # Stage the changes (updated badge)
+        stage_file(readme_path)
 
-    # Push changes
-    push_changes()
+        # Commit changes
+        commit_changes(message=f"Updated coverage badge in {readme_path}")
+
+        # Push changes
+        push_changes()
 
 
 def stage_file(file_path: Path):
