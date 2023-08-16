1e58e2e78355c7916b26f3cf8c57737f260d67f5
Joseph Crispell <crispelljoseph@gmail.com>
2023-08-16 19:36:31 +0100

    Added placeholder for testing git functions

---
+++ tests/test_git_functions.py
@@ -0,0 +1,22 @@
+# Load packages
+import unittest  # running tests
+from pathlib import Path  # handling file paths
+
+# Local imports
+from python_coverage_badge import (
+    git_functions,
+)  # functions foir interacting with git
+
+
+# TODO add tests
+
+
+class TestGitFunctions(unittest.TestCase):
+    def test_check_if_file_changed_using_git(self):
+
+        # TODO Add test in here!
+        print("hello! Tests to add!")
+
+
+if __name__ == "__main__":
+    unittest.main()
